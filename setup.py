6039feae838a50677e1098c4c1acc0a2c56fb8f3
Low Kian Seong <kianseong@gmail.com>
2014-08-13 06:31:07 +0800

    Update setup.py
    Adding more metadata to `setup.py`

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ if __name__ == '__main__':
     setup(name='formskit',
           version='0.4.1',
           author=['Dominik "Socek" Długajczy'],
+          url='https://github.com/socek/formskit/',
+          description='Simple forms managment.',
           author_email=['msocek@gmail.com', ],
           packages=find_packages(),
           test_suite='formskit.tests.get_all_test_suite',
